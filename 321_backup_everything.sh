7a7859ede5c5d90497d24431b023075c615fc032
Jake Martin <jm9357481@gmail.com>
2023-01-08 17:05:59 +0000

    Update 321_backup_everything.sh

--- 321_backup_everything.sh
+++ 321_backup_everything.sh
@@ -4,6 +4,7 @@
 #
 DEST1=$1
 DEST2=$2
+DEST3=$3
 
 if [ ! -x "$(command -v rsync)" ]; then
     echo rsync is not installed. ; exit 1
@@ -11,3 +12,4 @@ fi
 
 [ $DEST1 ] && rsync -aAXv / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} $DEST1
 [ $DEST2 ] && rsync -aAXv / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} $DEST2
+[ $DEST3 ] && rsync -aAXv / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} $DEST3
