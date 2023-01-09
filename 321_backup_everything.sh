08772847a688f3ae62d4724d341225eb9208931c
Jake Martin <jm9357481@gmail.com>
2023-01-09 15:29:38 -0800

    add check for root user or sudoer

--- 321_backup_everything.sh
+++ 321_backup_everything.sh
@@ -7,8 +7,10 @@ DEST2=$2
 DEST3=$3
 
 if [ ! -x "$(command -v rsync)" ]; then
-    echo rsync is not installed. ; exit 1
+    echo "rsync is not installed."; exit 1
 fi
+
+if [ "$(id -u)" != "0" ]; then echo "Run as root or use sudo/doas."; exit 1; fi
 
 [ $DEST1 ] && rsync -aAXv / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} $DEST1
 [ $DEST2 ] && rsync -aAXv / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} $DEST2
